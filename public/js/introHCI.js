e99eb0571cd5c4317be20f81bfe5ca67ceb03bff
Gabriel Aganon <>
2021-02-25 10:53:19 -0800

    Attended Lab 8, but I partially completed the Camera and did not start the RSVP

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,15 +10,23 @@ $(document).ready(function() {
  */
 function initializePage() {
 	console.log("Page ready");
- 	// initCamera();
- 	// initMap();
+ 	initCamera();
+ 	initMap();
  	initGestures();
  	initRSVPForm();
 }
 
-// init jQuery gestures  
+// init jQuery gestures
 function initGestures() {
 	// add gestures listener here
+    $(".judge-img").bind("taphold", tapholdHandler);
+    function tapholdHandler(event) {
+      // Getting the id of an event source
+      var targetIDPrefix = event.target.id;
+      console.log("got prefix: " + targetIDPrefix);
+      // Showing bio
+      $("#" + targetIDPrefix + "-bio").show();
+    };
 }
 
 // init RSVP form submit listener
--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,10 @@
 function initMap() {
-	// add your code here
+  L.mapquest.key = 'O3XedOqZs8NIkNsDruvgbas76IGG9Uh2';
+// 'map' refers to a <div> element with the ID map
+  var map = L.mapquest.map('map', {
+    center: [32.8785248,-117.2380747],
+    layers: L.mapquest.tileLayer('map'),
+    zoom: 12
+  });
+  L.marker([32.8785248,-117.2380747]).addTo(map);
 }
